8b1ef36a2b6edab7ad62a7ab3c11e6fb5b307ac9
zoravh <114677136+zoravh@users.noreply.github.com>
2024-04-27 13:15:45 +0000

    Hello Page Text 01

--- Hello.py
+++ Hello.py
@@ -12,23 +12,26 @@ def run():
 
     st.write("# Welcome to your online Bartender! 👋")
 
-    st.sidebar.success("Select a demo above.")
+    st.sidebar.success("Select a feature above.")
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Bla Bla subtitle
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+        Welcome to your gateway to mastering the art of cocktail making. 
+        By using this app, you'll not only broaden your drinking horizons and refine your palate but also dazzle your friends with your newfound mixology skills.
+        
+        This app was crafted by three university students eager to expand their horizons beyond the routine gin and tonics and screwdrivers. 
+        Through developing this app, they broke free from the mundane, embracing a world of vibrant and diverse cocktails.
+        
+        ## How Does This App Work?
+        The app boasts two core features designed to help you discover your perfect cocktail:
+        
+        ### Cocktail Search
+        - **Search Function**: Allows you to look up a cocktail by name. If it exists in our extensive database, you can view detailed information including ingredients, preparation instructions, ideal glassware, and additional tips.
+        
+        ### Cocktail Filter
+        - **Filter Function**: Enables you to find cocktails based on specific criteria such as the number of ingredients, preferred alcohol base, desired glassware, and the overall vibe of the cocktail. After setting your filters, the app will display all matching cocktails. Just click on one to see more about it, similar to the search function.
+        
+        Dive in and start exploring the rich world of cocktails with us!  
     """
     )
 
